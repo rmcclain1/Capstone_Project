7933602236c17f9035d3f9e6c8a9796f9249185a
JP <jpvictory0007@gmail.com>
2025-10-01 17:26:59 -0500

    Added a service to download the food recall records from FDA, and to populate the table food_events.

---
+++ RecallBackend/app/services/food_event_importer.rb
@@ -0,0 +1,72 @@
+# app/services/food_event_importer.rb
+
+require 'open-uri'
+require 'zip'
+require 'json'
+
+class FoodEventImporter
+  ZIP_URL = "https://download.open.fda.gov/food/enforcement/food-enforcement-0001-of-0001.json.zip"
+
+  def self.call
+    new.call
+  end
+
+  def call
+    download_and_extract do |json_data|
+      import_json_data(json_data)
+    end
+  end
+
+  private
+
+  def download_and_extract
+    file = URI.open(ZIP_URL)
+
+    Zip::File.open_buffer(file) do |zip_file|
+      zip_file.each do |entry|
+        next unless entry.name.ends_with?(".json")
+
+        json_content = entry.get_input_stream.read
+        data = JSON.parse(json_content)
+        yield(data)
+      end
+    end
+  end
+
+  def import_json_data(data)
+    events = data["results"] || []
+
+    events.each do |event|
+    FoodEvent.create!(
+        event_id: event["event_id"],
+        recall_number: event["recall_number"],
+        status: event["status"],
+        recalling_firm: event["recalling_firm"],
+        address_1: event["address_1"],
+        address_2: event["address_2"],
+        city: event["city"],
+        state: event["state"],
+        postal_code: event["postal_code"],
+        country: event["country"],
+        classification: event["classification"],
+        voluntary_mandated: event["voluntary_mandated"],
+        initial_firm_notification: event["initial_firm_notification"],
+        distribution_pattern: event["distribution_pattern"],
+        product_description: event["product_description"],
+        product_quantity: event["product_quantity"],
+        reason_for_recall: event["reason_for_recall"],
+        product_type: event["product_type"],
+        recall_initiation_date: parse_date(event["recall_initiation_date"]),
+        center_classification_date: parse_date(event["center_classification_date"]),
+        report_date: parse_date(event["report_date"]),
+        code_info: event["code_info"]
+        )
+        end
+    end
+
+
+  def parse_date(date_str)
+    return nil unless date_str.present?
+    Date.strptime(date_str, "%Y%m%d") rescue nil
+  end
+end
--- RecallBackend/db/schema.rb
+++ RecallBackend/db/schema.rb
@@ -10,9 +10,37 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_09_25_003200) do
+ActiveRecord::Schema[8.0].define(version: 2025_10_01_203336) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
+
+  create_table "food_events", force: :cascade do |t|
+    t.string "event_id"
+    t.string "recall_number"
+    t.string "status"
+    t.string "recalling_firm"
+    t.string "address_1"
+    t.string "address_2"
+    t.string "city"
+    t.string "state"
+    t.string "postal_code"
+    t.string "country"
+    t.string "classification"
+    t.string "voluntary_mandated"
+    t.text "initial_firm_notification"
+    t.text "distribution_pattern"
+    t.text "product_description"
+    t.string "product_quantity"
+    t.string "reason_for_recall"
+    t.string "product_type"
+    t.date "recall_initiation_date"
+    t.date "center_classification_date"
+    t.date "report_date"
+    t.string "code_info"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_food_events_on_event_id"
+  end
 
   create_table "pantries", force: :cascade do |t|
     t.integer "user_id"
@@ -29,16 +57,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_09_25_003200) do
     t.datetime "updated_at", null: false
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "birthday"
-    t.bigint "phone_number"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.string "password_digest"
+  create_table "users", id: :serial, force: :cascade do |t|
+    t.string "username", limit: 255, null: false
+    t.string "password_digest", limit: 255, null: false
+    t.string "first_name", limit: 255
+    t.string "last_name", limit: 255
+    t.string "email", limit: 255, null: false
+    t.date "birthday"
+    t.bigint "phonenumber"
+    t.datetime "created_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }, null: false
+    t.datetime "updated_at", precision: nil, default: -> { "CURRENT_TIMESTAMP" }, null: false
+
+    t.unique_constraint ["email"], name: "users_email_key"
+    t.unique_constraint ["username"], name: "users_username_key"
   end
 end
