76843d330461c720cd96b228ba23aa173d78287a
ToastyyX <erick.cool321@icloud.com>
2025-10-31 07:10:49 -0500

    Included the ability to change your password

--- mobile/app/(tabs)/settings.tsx
+++ mobile/app/(tabs)/settings.tsx
@@ -23,6 +23,17 @@ export default function SettingsScreen() {
   const [loading, setLoading] = useState(false);
   const { logout } = useAuth();
 
+  const transition_to_reset_password = async () => {
+    setLoading(true);
+    try {
+      router.push('/auth/reset-password');
+    } catch (e : any) { 
+      Alert.alert('Routing Failed');
+    } finally { 
+      setLoading(false);
+    }
+  };
+
   const handleLogout = async () => {
     setLoading(true);
     try {
@@ -48,7 +59,7 @@ export default function SettingsScreen() {
 
       <ScrollView contentContainerStyle={{ paddingBottom: 80 }}>
         <Text style={s.section}>Account</Text>
-        <Row label="Change Password" onPress={() => { }} />
+        <Row label="Change Password" onPress={transition_to_reset_password} />
         <Row label="Privacy and Security" onPress={() => { }} />
         <Row label="Log Out" onPress={handleLogout} />
 
