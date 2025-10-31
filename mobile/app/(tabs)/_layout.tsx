447653e8559da58429294128adb02a3434540ff7
ToastyyX <erick.cool321@icloud.com>
2025-10-31 07:07:21 -0500

    Changed from 'notifications' to 'alerts'

--- mobile/app/(tabs)/_layout.tsx
+++ mobile/app/(tabs)/_layout.tsx
@@ -59,7 +59,7 @@ export default function Layout() {
       <Tabs.Screen
         name="notifications"
         options={{
-          title: 'Notifications',
+          title: 'Alerts',
           tabBarIcon: ({ color, size }) => (
             <Ionicons name="notifications-outline" size={size} color={color} />
           ),
