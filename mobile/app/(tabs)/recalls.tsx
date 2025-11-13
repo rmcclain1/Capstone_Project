6ffc1e2e243c77a1581057c24ca9623e1b583645
JP <jpvictory0007@gmail.com>
2025-11-12 19:18:08 -0600

    Forgot another import item

--- mobile/app/(tabs)/recalls.tsx
+++ mobile/app/(tabs)/recalls.tsx
@@ -10,6 +10,7 @@ import {
     TextInput,
     Pressable,
     Platform,
+    ActivityIndicator,
 } from 'react-native';
 import { useRouter } from 'expo-router';
 import { Ionicons } from '@expo/vector-icons';
