d94a806157ea0cddb08bd03463836eeac5e3651a
Khurshed Ergashbaev <xurshedergashboyev@gmail.com>
2025-11-14 00:21:33 -0600

    fixed notifications conflict and bug

--- RecallBackend/app/models/food_event.rb
+++ RecallBackend/app/models/food_event.rb
@@ -1,3 +1,17 @@
 class FoodEvent < ApplicationRecord
   validates :event_id, presence: true
+
+  after_commit :notify_users, on: :create
+
+    private
+
+    def notify_users
+      User.where.not(expo_push_token: nil).find_each do |user|
+        ExpoPushService.send_to(
+          user,
+          title: "New recall: #{title}",
+          body: issuer.present? ? "Issued by #{issuer}" : "A new recall was posted."
+        )
+      end
+    end
 end
